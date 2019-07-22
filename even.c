f4c7c34a8ed97b56f5cd99f0d772904174be8c32
kavin45 <52598647+kavin45@users.noreply.github.com>
2019-07-22 16:28:10 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ 
+int main()
+
+{
+    
+int n;
+    
+scanf("%d",&n);
+   
+ if(n%2==0)
+
+{    
+
+printf("even");
+        
+    
+    
+}
+   
+ else
+   
+ {
+       
+ printf("negative");
+   
+ }
+
+}
