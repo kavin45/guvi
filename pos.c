71cb5edc41b1128968a7e26e3e6ece3d3e2ffbc9
kavin45 <52598647+kavin45@users.noreply.github.com>
2019-07-22 16:02:20 +0530

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+ 
+int main()
+
+{
+    
+int n;
+    
+scanf("%d",&n);
+    
+if(n==0)
+    
+{
+        
+printf("zero");
+    
+   
+ }
+    
+else if(n>0)
+    
+{
+        
+printf("positve");
+   
+ }
+    
+else
+    
+{
+        
+printf("negative");
+ 
+   }
+    
+return 0;
+
+}
