2289829780a215bc3e37b836692326507e2f213a
kavin45 <52598647+kavin45@users.noreply.github.com>
2019-07-24 16:06:52 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+     fact = 1;
+  
+    scanf("%d",&n);
+    
+    if (n < 0)
+       
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            fact *= i;             
+        }
+        printf(" %d ", n, fact);
+    }
+    return 0;
+}
